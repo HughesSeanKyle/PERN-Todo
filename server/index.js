fad61a897254d54db286a5ef5240508f859dc593
HughesSeanKyle <khughessean@yahoo.com>
2021-12-19 11:04:46 +0200

    index.js update

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const cors = require('cors');
 
-// middleware
+// **MIDDLEWARE
 app.use(cors());
+// Gives us access to req.body converted to json
+app.use(express.json());
 
 // Start server on PORT 5000
 app.listen(5000, () => {
