92d900de0c84da56b62b2b7e4aa8716961dd0964
HughesSeanKyle <khughessean@yahoo.com>
2021-12-19 12:11:02 +0200

    Set up db config and linked to index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-// Make express Lib run
 const app = express();
-
 const cors = require('cors');
+const pool = required('./db');
 
 // **MIDDLEWARE
 app.use(cors());
 // Gives us access to req.body converted to json
 app.use(express.json());
+
+// **ROUTES
+// Create a todo (POST)
+// get all todos (GET)
+// get a todo (GET)
+// update a todo (PUT)
+// delete a todo (DELETE)
 
 // Start server on PORT 5000
 app.listen(5000, () => {
